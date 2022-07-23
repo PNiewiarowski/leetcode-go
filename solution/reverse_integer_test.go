e29065e18ba6ce8eff5104097b5857a4b508adf3
PNiewiarowski <pawelniewiarowski2001@gmail.com>
2022-07-23 11:33:57 +0200

    Refactor unit test/ReverseInteger

--- solution/reverse_integer_test.go
+++ solution/reverse_integer_test.go
@@ -2,34 +2,40 @@ package solution
 
 import (
 	"math"
+	"reflect"
 	"testing"
 )
 
-type testCase struct {
-	val  int
-	want int
-}
-
 func TestReverseInteger(t *testing.T) {
-	testCases := []testCase{
+	type test struct {
+		val  int
+		want int
+		name string
+	}
+
+	tests := []test{
 		{
 			val:  123,
 			want: 321,
+			name: "Reverse Integer 001",
 		},
 		{
 			val:  -456,
 			want: -654,
+			name: "Reverse Integer 002",
 		},
 		{
 			val:  int(math.Pow(2, 33)),
 			want: 0,
+			name: "Reverse Integer 003",
 		},
 	}
 
-	for _, tc := range testCases {
-		got := ReverseInteger(tc.val)
-		if got != tc.want {
-			t.Errorf("\nGot:\t %d\nWant:\t %d", got, tc.want)
-		}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if got := ReverseInteger(tc.val); !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("\nGot:\t %d\nWant:\t %d", got, tc.want)
+			}
+		})
 	}
 }
